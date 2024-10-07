5a099e5c16edd2025115de8904d5b87b1bf71554
KunalBishwal <159259649+KunalBishwal@users.noreply.github.com>
2024-10-07 22:47:31 +0530

    Create RoundedBorder.java

---
+++ RoundedBorder.java
@@ -0,0 +1,25 @@
+import javax.swing.border.AbstractBorder;
+import java.awt.*;
+
+public class RoundedBorder extends AbstractBorder {
+    private final int radius;
+
+    public RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius, radius, radius, radius);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return true;
+    }
+}
